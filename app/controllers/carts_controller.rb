57de12fd6791c4c6a7fa49e868a5113ad26f30eb
cool8676 <cool8676@gmail.com>
2020-03-01 23:11:18 +0800

    complete add item to cart

--- app/controllers/carts_controller.rb
+++ app/controllers/carts_controller.rb
@@ -1,2 +1,10 @@
 class CartsController < ApplicationController
+
+  def add
+    cart = Cart.from_hash(session[:cart0001])
+    cart.add_item(params[:id])
+    session[:cart0001] = cart.serialize
+
+    redirect_to products_path, notice: "已加入購物車"
+  end
 end
