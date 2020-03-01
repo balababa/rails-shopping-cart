d48a7f7b10e5a18a845ad0e0bed4be8ea42f5c29
cool8676 <cool8676@gmail.com>
2020-03-02 02:37:30 +0800

    refactor get cart from hash

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,10 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
+
+  before_action :init_cart #其他controller可能也會用到，故放在上層controller
+  include CartsHelper #current_cart in helper
+  private
+  def init_cart
+    @cart = Cart.from_hash(session[:cart9487])
+  end
 end
--- app/controllers/carts_controller.rb
+++ app/controllers/carts_controller.rb
@@ -1,10 +1,12 @@
 class CartsController < ApplicationController
 
   def add
-    cart = Cart.from_hash(session[:cart0001])
-    cart.add_item(params[:id])
-    session[:cart0001] = cart.serialize
+    @cart.add_item(params[:id])
+    session[:cart0001] = @cart.serialize
 
+    # current_cart.add_item(params[:id])
+    # session[:cart0001] = current_cart   #in module carts_helper
+    
     redirect_to products_path, notice: "已加入購物車"
   end
 end
