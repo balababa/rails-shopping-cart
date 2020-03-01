ad89fc90b9dd51273593d41e2716cb8c920b2679
cool8676 <cool8676@gmail.com>
2020-03-01 15:21:59 +0800

    Add test 8

--- app/models/cart.rb
+++ app/models/cart.rb
@@ -1,14 +1,14 @@
 class Cart
   attr_reader :items
 
-  def initialize()
-    @items = []  
+  def initialize(items = [])
+    @items = items  
   end
 
   def add_item(product_id)
     item_found = items.find {|item| item.product_id == product_id }
     
-    if item_found
+    if item_found 
       item_found.increment
     else
       items << CartItem.new(product_id)
@@ -34,5 +34,13 @@ class Cart
     }
 
     { "items" => all_items }
+  end
+
+  def self.from_hash(hash)
+    if hash.nil?
+      new 
+    else
+      new hash["items"].map {|item| CartItem.new(item["product_id"], item["quantity"])}
+    end
   end
 end
